58c7bcd8722e1b82a32ff688525f08721e49b0f6
Germano Ricardi <germanoricardi7@gmail.com>
2026-04-14 23:14:30 -0400

    feat(auth): integrate next-auth for authentication and enhance LoginForm with error handling

---
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,78 @@
+import NextAuth from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+
+const handler = NextAuth({
+  providers: [
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        email: { label: "Email", type: "text" },
+        password: { label: "Senha", type: "password" },
+      },
+
+      async authorize(credentials) {
+        try {
+          const res = await fetch("http://cartorio-digital-core:3000/v1/auth/login", {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+              email: credentials?.email,
+              password: credentials?.password,
+            }),
+          });
+
+          const data = await res.json();
+
+          console.log("Resposta do backend:", { res, data });
+
+          // 👉 ajuste conforme padrão do seu backend
+          if (res.status != 200) {
+            throw new Error(data.message || "Erro ao autenticar");
+          }
+
+          // 👉 importante: retornar o usuário
+          return {
+            id: data.user.id,
+            name: data.user.name,
+            email: data.user.email,
+            accessToken: data.accessToken,
+          };
+        } catch (error) {
+          console.log("Erro na autorização:", {error});
+          throw new Error(error instanceof Error ? error.message : "Erro desconhecido");
+        }
+      },
+    }),
+  ],
+
+  session: {
+    strategy: "jwt",
+  },
+
+  callbacks: {
+    async jwt({ token, user }) {
+      // primeira vez (login)
+      if (user) {
+        token.accessToken = user.accessToken;
+        token.user = user;
+      }
+
+      return token;
+    },
+
+    async session({ session, token }) {
+      session.user = token.user;
+      session.accessToken = token.accessToken;
+
+      return session;
+    },
+  },
+
+  pages: {
+    signIn: "/", // opcional
+  },
+});
+
+export { handler as GET, handler as POST };
--- src/modules/auth/components/LoginForm.tsx
+++ src/modules/auth/components/LoginForm.tsx
@@ -1,14 +1,19 @@
 'use client';
 
-import { Button } from "@mui/material";
+import { Alert, Button } from "@mui/material";
 import { Grid, Link, TextField } from "@mui/material";
 import { useAuth } from "../hooks/useAuth";
 import { useForm, Controller } from "react-hook-form";
 import { loginSchema, LoginSchema } from "../schemas/login.schema";
 import { zodResolver } from "@hookform/resolvers/zod";
+import { signIn } from "next-auth/react";
+import { useState } from "react";
+import { set } from "lodash";
 
 export function LoginForm() {
-  const { login, loading } = useAuth();
+  const { login } = useAuth();
+  const [loading, setLoading] = useState<string | boolean>(false)
+  const [error, setError] = useState<string | boolean>(false)
 
   const {
     control,
@@ -23,12 +28,30 @@ export function LoginForm() {
   });
 
   async function onSubmit(data: LoginSchema) {
-    await login(data);
+    setError(false);
+    setLoading(true);
+
+    const res = await signIn("credentials", {
+      ...data,
+      redirect: false,
+    })
+    .finally(() => setLoading(false));
+    
+    if(res?.error)
+      setError(res.error ?? "Erro realizar o login. Verifique os dados.");
   }
 
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
       <Grid container direction="column" gap={2}>
+
+        {
+          error && (
+            <Grid>
+              <Alert severity="error" variant="outlined">{error}</Alert>
+            </Grid>
+          )
+        }
         
         <Grid>
           <Controller
