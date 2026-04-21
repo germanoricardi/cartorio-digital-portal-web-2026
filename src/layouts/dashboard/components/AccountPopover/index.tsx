460f91b75f88af43a774213b17ad37119c4e6cae
Germano Ricardi <germanoricardi7@gmail.com>
2026-04-21 05:37:46 -0400

    feat: implement CustomPopover component and integrate it into AccountPopover and Header layouts

---
+++ src/layouts/dashboard/components/AccountPopover/index.tsx
@@ -0,0 +1,76 @@
+import CustomPopover, { usePopover } from "@/components/CustomPopover";
+import { Avatar, Box, Divider, IconButton, MenuItem, Stack, Typography } from "@mui/material";
+import { signOut, useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
+
+const OPTIONS = [
+  {
+    label: 'Perfil',
+    linkTo: '/profile',
+  },
+];
+
+export default function AccountPopover() {
+  const router = useRouter();
+  const popover = usePopover();
+  const { data } = useSession();
+
+  const handleClickItem = (path: string) => {
+    popover.onClose();
+    router.push(path);
+  };
+
+  return (
+    <>
+      <IconButton
+        onClick={popover.onOpen}
+      >
+        <Avatar
+          sx={{
+            width: 36,
+            height: 36,
+            border: (theme) => `solid 2px ${theme.palette.background.paper}`,
+          }}
+        >
+          { data?.user.name?.charAt(0).toUpperCase() }
+        </Avatar>
+      </IconButton>
+
+      <CustomPopover
+        open={popover.open}
+        onClose={popover.onClose}
+        sx={{ width: 200, p: 0 }}
+      >
+        <Box sx={{ p: 2, pb: 1.5 }}>
+          <Typography variant="subtitle2" noWrap>
+            {data?.user?.name}
+          </Typography>
+
+          <Typography variant="body2" sx={{ color: 'text.secondary' }} noWrap>
+            {data?.user?.email}
+          </Typography>
+        </Box>
+
+        <Divider sx={{ borderStyle: 'dashed' }} />
+
+        <Stack sx={{ p: 1 }}>
+          {OPTIONS.map((option) => (
+            <MenuItem key={option.label} onClick={() => handleClickItem(option.linkTo)}>
+              {option.label}
+            </MenuItem>
+          ))}
+        </Stack>
+
+        <Divider sx={{ borderStyle: 'dashed' }} />
+
+        <MenuItem
+          onClick={() => signOut()}
+          sx={{ m: 1, fontWeight: 'fontWeightBold', color: 'error.main' }}
+        >
+          Sair
+        </MenuItem>
+
+      </CustomPopover>
+    </>
+  )
+}
--- src/layouts/dashboard/components/Header/index.tsx
+++ src/layouts/dashboard/components/Header/index.tsx
@@ -1,5 +1,7 @@
-import SvgColor from "@/components/SvgColor/SvgColor";
+
+import SvgColor from "@/components/SvgColor";
 import { AppBar, IconButton, Toolbar } from "@mui/material";
+import AccountPopover from "../AccountPopover";
 
 export default function Header() {
   return (
@@ -8,6 +10,8 @@ export default function Header() {
         <IconButton>
           <SvgColor src="/assets/icons/navbar/ic_menu_item.svg" />
         </IconButton>
+
+        <AccountPopover />
       </Toolbar>
     </AppBar>
   )
